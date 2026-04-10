967015573a4761513784a014b7c496e6fd312001
Viacheslav Vladimirov <slava.a.vladimirov@gmail.com>
2026-04-10 16:24:32 +0200

    v1.0

--- controller.py
+++ controller.py
@@ -1,47 +1,230 @@
 import model
 import datetime
-import json
+import mysql.connector
+
+PRODUCT_SELECT = "SELECT DISTINCT product_name, product_price, product_stock, product_id FROM project.products"
+PRODUCT_INSERT = "INSERT INTO project.products (product_name, product_price, product_stock) VALUES (%s, %s, %s)"
+PRODUCT_UPDATE = "UPDATE project.products SET product_name = %s, product_price = %s, product_stock = %s WHERE product_id = %s"
+PRODUCT_DELETE = "DELETE FROM project.products WHERE product_id = %s"
+
+CUSTOMER_SELECT = "SELECT customer_name, customer_email, customer_id FROM project.customers"
+CUSTOMER_INSERT = "INSERT INTO project.customers (customer_name, customer_email) VALUES (%s, %s)"
+CUSTOMER_UPDATE = "UPDATE project.customers SET customer_name = %s, customer_email = %s WHERE customer_id = %s"
+CUSTOMER_DELETE = "DELETE FROM project.customers WHERE customer_id = %s"
+
+ORDERS_SELECT = "SELECT sales.sale_id, customers.customer_name, sales.isPaid, sales.total, sales.sale_date FROM project.sales JOIN project.customers ON sales.customer_id = customers.customer_id WHERE sale_status='order' AND isDelivered = FALSE"
+ORDERLINE_SELECT = "SELECT products.product_name, salelines.quantity FROM project.salelines JOIN project.products ON salelines.product_id = products.product_id WHERE sale_id = %s"
+ORDER_INSERT = "INSERT INTO project.sales (customer_id, isPaid, isDelivered, total, sale_date, sale_status) VALUES (%s, %s, False, %s, %s, 'order')"
+
+SALELINE_INSERT = "INSERT INTO project.salelines (sale_id, product_id, quantity) VALUES (%s, %s, %s)"
+
+SALE_INSERT = "INSERT INTO project.sales (customer_id, isPaid, isDelivered, total, sale_date, sale_status) VALUES (%s, True, True, %s, %s, 'sale')"
+
+PAYMENT_INSERT = "INSERT INTO project.payments (sale_id, amount) VALUES (%s, %s)"
+
 
 class Controller:
     def __init__(self):
-        self.customers = []
-        self.orders = []
-        self.sales = []
-        self.products = []
-        self.payments = []
-        self.saleItems = []
+        self.selected_customer = None
+        self.current_operation = []
+        self.isPaid = False
+        self.currentOrderId = None
+        self.selected_product_row = None
 
-    def generateId(self, objList):
-        lastId = 0
+        print("Before DB connect")
+        try:
+            self.DB = mysql.connector.connect(
+                                host="127.0.0.1",
+                                user="root",
+                                password="password",
+                                database="project",
+                                connection_timeout=3,
+                                use_pure=True)
+            self.cursor = self.DB.cursor()
+            print("After DB connect")
+        except Exception as e:
+             print("error: ", e)
 
-        for obj in objList:
-            if obj.id > lastId: 
-                lastId = obj.id
+    def listProducts(self):
+        self.cursor.execute(PRODUCT_SELECT)
+        rows = self.cursor.fetchall()
 
-        id = lastId + 1
+        return rows
+    
+    def listCustomers(self):
+        self.cursor.execute(CUSTOMER_SELECT)
+        rows = self.cursor.fetchall()
+
+        return rows
+    
+    def registerCustomer(self, name, email):
+        self.cursor.execute("SELECT customer_name, customer_email FROM project.customers WHERE customer_name = %s OR customer_email = %s", (name, email))
+
+        if self.cursor.fetchone():
+            return False
+        else:
+            try:
+                self.cursor.execute(CUSTOMER_INSERT, (name, email))
+                self.DB.commit()
+                return True
+            except Exception as e:
+                print("error: ", e)
+                return False
+            
+    def updateCustomer(self, name, email, id):
+        try:
+            self.cursor.execute(CUSTOMER_UPDATE, (name, email, id))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+        
+    def deleteCustomer(self, id):
+        try:
+            self.cursor.execute(CUSTOMER_DELETE, (id,))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+            
+    def set_customer(self, name, email, id):
+        self.selected_customer = (name, email, id)
+
+    def clearCustomer(self):
+        self.selected_customer = None
+
+    def addToOperation(self, product, quantity):
+        self.current_operation.append({"product": product, "quantity": quantity})
+
+    def clearOperation(self):
+        self.current_operation = []
+        self.isPaid = False
+
+    def listOrders(self):
+        self.cursor.execute(ORDERS_SELECT)
+        rows = self.cursor.fetchall()
+
+        return rows
+    
+    def getSaleLines(self, saleId):
+        self.cursor.execute(ORDERLINE_SELECT, (saleId,))
+        rows = self.cursor.fetchall()
+
+        return rows
+    
+    def registerOrder(self, customer_id, isPaid, total):
+        try:
+            date = datetime.datetime.now()
+
+            self.cursor.execute(ORDER_INSERT, (customer_id, isPaid, total, date))
+            order_id = self.cursor.lastrowid
+
+            data = [
+            (
+                order_id,
+                item["product"].id,
+                item["quantity"]
+            )
+            for item in self.current_operation
+            ]
+
+            self.cursor.executemany(SALELINE_INSERT, data)
+            self.DB.commit()
+
+            return True
+        except Exception as e:
+            print("Error: ",e)
+            return False
+        
+    def getProductId(self, name):
+        self.cursor.execute("SELECT product_id FROM project.products WHERE product_name = %s", (name,))
+        id = self.cursor.fetchone()[0]
+
         return id
+    
+    def registerSale(self, total, customer_id=None):
+        try:
+            date = datetime.datetime.now()
 
-    def createOrder(self, customer_id, isPaid, isDone, orderItem_id, payment_id, totalAmount):
-        id = self.generateId(self.orders)
-        date = datetime.date.today
-        order = model.Order(id, customer_id, isPaid, isDone, orderItem_id, payment_id, totalAmount, date)
+            self.cursor.execute(SALE_INSERT, (customer_id, total, date))
+            sale_id = self.cursor.lastrowid
 
-        self.orders.append(order)
+            payment_success = self.registerPayment(sale_id, total)
 
-    def sale(self, saleItem_id, payment_id): #fix the saleItem_id and payment_id
-        id = self.generateId(self.sales)
-        date = datetime.date.today
-        sale = model.Sale(id, saleItem_id, payment_id, date)
+            data = [
+            (
+                sale_id,
+                item["product"].id,
+                item["quantity"]
+            )
+            for item in self.current_operation
+            ]
 
-        self.sales.append(sale)
+            self.cursor.executemany(SALELINE_INSERT, data)
+            if payment_success: self.DB.commit()
+            else: return False
 
-    def registerPayment(self):
-        id = self.generateId(self.payments)
-        payment = model.Payment()
-
-    def registerSaleItem(self, product_id, quantity):
-        id = self.generateId(self.saleItems)
-        sale_id = self.generateId(self.sales)
-        saleItem = model.SaleItem(id, sale_id, product_id, quantity)
-
-        self.saleItems.append(saleItem)
+            return True
+        except Exception as e:
+            print("Error: ",e)
+            return False
+        
+    def setPaid(self):
+        self.isPaid = True
+        return True
+    
+    def updatePaymentStatus(self, sale_id):
+        try:
+            self.cursor.execute("UPDATE project.sales SET isPaid = True WHERE sale_id = %s", (sale_id,))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+        
+    def completeOrder(self, sale_id):
+        try:
+            self.cursor.execute("UPDATE project.sales SET isDelivered = True WHERE sale_id = %s", (sale_id,))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+        
+    def registerPayment(self, sale_id, amount):
+        try:
+            self.cursor.execute(PAYMENT_INSERT, (sale_id, amount))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+        
+    def registerProduct(self, name, price, stock):
+        try:
+            self.cursor.execute(PRODUCT_INSERT, (name, price, stock))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+        
+    def updateProduct(self, name, price, stock):
+        try:
+            self.cursor.execute(PRODUCT_UPDATE, (name, price, stock, self.selected_product_row.product_id))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
+        
+    def deleteProduct(self):
+        try:
+            self.cursor.execute(PRODUCT_DELETE, (self.selected_product_row.product_id,))
+            self.DB.commit()
+            return True
+        except Exception as e:
+            print("error: ", e)
+            return False
--- model.py
+++ model.py
@@ -4,37 +4,22 @@ class Customer:
         self.name = name
         self.email = email
 
-class Order:
-    def __init__(self, id, customer_id, isPaid, isDone, orderItem_id, payment_id, totalAmount, date):
+class Sale:
+    def __init__(self, id, customer_id, isPaid, isDelivered, totalAmount, date, status):
         self.id = id
         self.customer_id = customer_id
         self.isPaid = isPaid
-        self.isDone = isDone
-        self.orderItem_id = orderItem_id
-        self.payment_id = payment_id
+        self.isDelivered = isDelivered
         self.totalAmount = totalAmount
         self.date = date
-
-class OrderItem:
-    def __init__(self, id, order_id, product_id, quantity):
-        self.id = id
-        self.order_id = order_id
-        self.product_id = product_id
-        self.quantity = quantity
+        self.status = status
+        self.items = []
 
 class SaleItem:
-    def __init__(self, id, sale_id, product_id, quantity):
+    def __init__(self, id, product_id, quantity):
         self.id = id
-        self.sale_id = sale_id
         self.product_id = product_id
         self.quantity = quantity
-
-class Sale:
-    def __init__(self, id, saleItem_id, payment_id, date):
-        self.id = id
-        self.SaleItem_id = saleItem_id
-        self.payment_id = payment_id
-        self.date = date
 
 class Product:
     def __init__(self, id, name, price, stock):
@@ -44,8 +29,7 @@ class Product:
         self.stock = stock
 
 class Payment:
-    def __init__(self, id, sale_id, order_id, amount):
+    def __init__(self, id, sale_id, amount):
         self.id = id
         self.sale_id = sale_id
-        self.order_id = order_id
         self.amount = amount
